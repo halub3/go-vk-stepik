3fb839b78cd71b3a1f821729d7d0a471b93abff8
Sushchenko <sushchenko.aa@phystech.edu>
2025-01-05 18:54:57 +0300

    hw 1 DONE

--- 1/99_hw/tree/main.go
+++ 1/99_hw/tree/main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
-	"strings"
+)
+
+var (
+	Trait    string = "├───"
+	HTrait   string = "└───"
+	Stick    string = "│"
+	StickTab string = "│\t"
+	Tab      string = "\t"
 )
 
 func main() {
@@ -19,4 +25,69 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func dirTree(w io.Writer, dir string, isFiles bool) (err error) {
+	return dirTreeRec(w, dir, isFiles, "")
+}
+
+func dropFiles(in []os.DirEntry) []os.DirEntry {
+	var res []os.DirEntry
+	for _, v := range in {
+		if v.IsDir() {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func dirTreeRec(w io.Writer, dir string, isFiles bool, prefix string) (err error) {
+
+	objs, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	if !isFiles {
+		objs = dropFiles(objs)
+	}
+
+	lastIdx := len(objs) - 1
+	for idx, obj := range objs {
+		var startString, nextPrefix string
+
+		isLast := idx == lastIdx
+
+		if isLast {
+			startString = prefix + HTrait
+			nextPrefix = prefix + Tab
+		} else {
+			startString = prefix + Trait
+			nextPrefix = prefix + StickTab
+		}
+
+		fmt.Fprint(w, startString+obj.Name())
+
+		if obj.IsDir() {
+			fmt.Fprint(w, "\n")
+			err = dirTreeRec(w, dir+"\\"+obj.Name(), isFiles, nextPrefix)
+			if err != nil {
+				return
+			}
+		} else {
+			info, err := obj.Info()
+			if err != nil {
+				return err
+			}
+			size := info.Size()
+			if size > 0 {
+				fmt.Fprint(w, " (", size, "b)\n")
+			} else {
+				fmt.Fprint(w, " (empty)\n")
+			}
+		}
+	}
+
+	return
+
 }
